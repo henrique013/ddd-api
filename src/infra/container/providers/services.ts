6d32b992d378154bdb75aee20db9390343d0c1b1
Henrique Alves <henrique013.a@gmail.com>
2025-06-07 13:20:10 -0300

    feat(container): add repository registration and update service dependency

--- src/infra/container/providers/services.ts
+++ src/infra/container/providers/services.ts
@@ -2,13 +2,11 @@ import { DependencyContainer } from 'tsyringe'
 import { t } from '@infra/container/tokens.js'
 import { CityService } from '@domain/services/city.js'
 import { CitiesSqliteRepo } from '@infra/repos/cities.sqlite.js'
-import { DrizzleSqlite } from '@infra/orm/types.js'
 
 export function registerServices(container: DependencyContainer) {
   container.register(t.services.ICityService, {
     useFactory: () => {
-      const db = container.resolve<DrizzleSqlite>(t.libs.DrizzleSqlite)
-      const citiesRepo = new CitiesSqliteRepo(db)
+      const citiesRepo = container.resolve<CitiesSqliteRepo>(t.repos.ICitiesRepo)
       return new CityService(citiesRepo)
     },
   })
