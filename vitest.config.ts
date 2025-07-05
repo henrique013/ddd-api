b7f35f70640f741542339602be88cc917442a9a9
Henrique Alves <henrique013.a@gmail.com>
2025-07-04 23:13:36 -0300

    feat(tests): move unit tests dir to project root

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,27 +7,19 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
-      exclude: ['node_modules/**', 'src/domain/tests/**', 'src/infra/**', './*.{ts,js}'],
+      exclude: [
+        './node_modules/**',
+        './*.{ts,js}',
+        './src/infra/{container,orm,repos,scripts,web-server}/**',
+        './src/infra/*.ts',
+      ],
     },
     workspace: [
       {
         test: {
-          name: 'domain:unit',
+          name: 'unit',
           environment: 'node',
-          include: ['src/domain/tests/unit/**/*.test.ts'],
-          globals: true,
-          testTimeout: 10000,
-          alias: {
-            '@domain': resolve(__dirname, './src/domain'),
-            '@infra': resolve(__dirname, './src/infra'),
-          },
-        },
-      },
-      {
-        test: {
-          name: 'infra:unit',
-          environment: 'node',
-          include: ['src/infra/tests/unit/**/*.test.ts'],
+          include: ['tests/unit/**/*.test.ts'],
           globals: true,
           testTimeout: 10000,
           alias: {
